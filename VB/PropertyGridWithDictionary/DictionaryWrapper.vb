abfda7e7e341c783ed0c4c41000cf0b0176d9ece
DevExpressExampleBot <fakeEmail>
2021-09-06 20:53:25 +0300

    autogenerate VB

--- VB/PropertyGridWithDictionary/DictionaryWrapper.vb
+++ VB/PropertyGridWithDictionary/DictionaryWrapper.vb
@@ -4,68 +4,68 @@ Imports System.Linq
 Imports System.ComponentModel
 
 Namespace PropertyGridWithDictionary
-    Friend Class DictionaryWrapper(Of T)
-        Implements ICustomTypeDescriptor
+	Friend Class DictionaryWrapper(Of T)
+		Implements ICustomTypeDescriptor
 
-        Public Dict As Dictionary(Of String, T)
+		Public Dict As Dictionary(Of String, T)
 
-        Private _collection As PropertyDescriptorCollection
+		Private _collection As PropertyDescriptorCollection
 
-        Public Sub New(ByVal dict As Dictionary(Of String, T))
-            Me.Dict = dict
-            Dim coll As New List(Of PropertyDescriptor)()
-            For Each key As String In Me.Dict.Keys
-                coll.Add(New CustomPropertyDescriptor(Of T)(GetType(DictionaryWrapper(Of T)), key, Me.Dict(key).GetType(), New Attribute() { }))
-            Next key
-            _collection = New PropertyDescriptorCollection(coll.ToArray())
-        End Sub
+		Public Sub New(ByVal dict As Dictionary(Of String, T))
+			Me.Dict = dict
+			Dim coll As New List(Of PropertyDescriptor)()
+			For Each key As String In Me.Dict.Keys
+				coll.Add(New CustomPropertyDescriptor(Of T)(GetType(DictionaryWrapper(Of T)), key, Me.Dict(key).GetType(), New Attribute() {}))
+			Next key
+			_collection = New PropertyDescriptorCollection(coll.ToArray())
+		End Sub
 
-        Public Function GetAttributes() As AttributeCollection Implements ICustomTypeDescriptor.GetAttributes
-            Return New AttributeCollection()
-        End Function
+		Public Function GetAttributes() As AttributeCollection
+			Return New AttributeCollection()
+		End Function
 
-        Public Function GetClassName() As String Implements ICustomTypeDescriptor.GetClassName
-            Return ""
-        End Function
+		Public Function GetClassName() As String
+			Return ""
+		End Function
 
-        Public Function GetComponentName() As String Implements ICustomTypeDescriptor.GetComponentName
-            Return ""
-        End Function
+		Public Function GetComponentName() As String
+			Return ""
+		End Function
 
-        Public Function GetConverter() As TypeConverter Implements ICustomTypeDescriptor.GetConverter
-            Return New TypeConverter()
-        End Function
+		Public Function GetConverter() As TypeConverter
+			Return New TypeConverter()
+		End Function
 
-        Public Function GetDefaultEvent() As EventDescriptor Implements ICustomTypeDescriptor.GetDefaultEvent
-            Return Nothing
-        End Function
+		Public Function GetDefaultEvent() As EventDescriptor
+			Return Nothing
+		End Function
 
-        Public Function GetDefaultProperty() As PropertyDescriptor Implements ICustomTypeDescriptor.GetDefaultProperty
-            Return _collection(0)
-        End Function
+		Public Function GetDefaultProperty() As PropertyDescriptor
+			Return _collection(0)
+		End Function
 
-        Public Function GetEditor(ByVal editorBaseType As Type) As Object Implements ICustomTypeDescriptor.GetEditor
-            Return New Object()
-        End Function
+		Public Function GetEditor(ByVal editorBaseType As Type) As Object
+			Return New Object()
+		End Function
 
-        Public Function GetEvents(ByVal attributes() As Attribute) As EventDescriptorCollection Implements ICustomTypeDescriptor.GetEvents
-            Return Nothing
-        End Function
+		Public Function GetEvents(ByVal attributes() As Attribute) As EventDescriptorCollection
+			Return Nothing
+		End Function
 
-        Public Function GetEvents() As EventDescriptorCollection Implements ICustomTypeDescriptor.GetEvents
-            Return Nothing
-        End Function
+		Public Function GetEvents() As EventDescriptorCollection
+			Return Nothing
+		End Function
 
-        Public Function GetProperties(ByVal attributes() As Attribute) As PropertyDescriptorCollection Implements ICustomTypeDescriptor.GetProperties
-            Return _collection
-        End Function
+		Public Function GetProperties(ByVal attributes() As Attribute) As PropertyDescriptorCollection
+			Return _collection
+		End Function
 
-        Public Function GetProperties() As PropertyDescriptorCollection Implements ICustomTypeDescriptor.GetProperties
-            Return _collection
-        End Function
+		Public Function GetProperties() As PropertyDescriptorCollection
+			Return _collection
+		End Function
 
-        Public Function GetPropertyOwner(ByVal pd As PropertyDescriptor) As Object Implements ICustomTypeDescriptor.GetPropertyOwner
-            Return Me
-        End Function
-    End Class
+		Public Function GetPropertyOwner(ByVal pd As PropertyDescriptor) As Object
+			Return Me
+		End Function
+	End Class
 End Namespace
--- VB/PropertyGridWithDictionary/MainWindow.xaml.vb
+++ VB/PropertyGridWithDictionary/MainWindow.xaml.vb
@@ -3,24 +3,24 @@ Imports System.Collections.Generic
 Imports System.Windows
 
 Namespace PropertyGridWithDictionary
-    Partial Public Class MainWindow
-        Inherits Window
+	Partial Public Class MainWindow
+		Inherits Window
 
-        Public Sub New()
-            InitializeComponent()
-        End Sub
-        Private Sub propertyGrid_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            Dim dict As New Dictionary(Of String, Object)()
-            dict.Add("FirstName", "Carolyn")
-            dict.Add("LastName", "Baker")
-            dict.Add("Email", "carolyn.baker@example.com")
-            dict.Add("Phone", "(555)349-3010")
-            dict.Add("Address", "1198 Theresa Cir")
-            dict.Add("City", "Whitinsville")
-            dict.Add("State", "MA")
-            dict.Add("Zip", "01582")
-            Dim wrapper As New DictionaryWrapper(Of Object)(dict)
-            propertyGrid.SelectedObject = wrapper
-        End Sub
-    End Class
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+		Private Sub propertyGrid_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim dict As New Dictionary(Of String, Object)()
+			dict.Add("FirstName", "Carolyn")
+			dict.Add("LastName", "Baker")
+			dict.Add("Email", "carolyn.baker@example.com")
+			dict.Add("Phone", "(555)349-3010")
+			dict.Add("Address", "1198 Theresa Cir")
+			dict.Add("City", "Whitinsville")
+			dict.Add("State", "MA")
+			dict.Add("Zip", "01582")
+			Dim wrapper As New DictionaryWrapper(Of Object)(dict)
+			propertyGrid.SelectedObject = wrapper
+		End Sub
+	End Class
 End Namespace
